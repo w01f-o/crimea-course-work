510091c160657a77e160d6d4fd532b8ae7569368
w01f <arsenimarchenko@bk.ru>
2024-05-22 12:51:29 +0300

    new pages

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,20 @@
-const burgerMenu = document.querySelector('.burger');
+const burgerMenu = document.querySelector(".burger");
 
-document.body.addEventListener('click', (e) => {
-    if (e.target.matches('.burger__button')) {
-        burgerMenu.classList.toggle('burger_open');
-    }
+document.body.addEventListener("click", (e) => {
+  if (e.target.matches(".burger__button")) {
+    burgerMenu.classList.toggle("burger_open");
+  }
 
+  if (
+    !e.target.matches(".burger__button") &&
+    !e.target.matches(".burger__menu")
+  ) {
+    burgerMenu.classList.remove("burger_open");
+  }
+});
 
-    if (!e.target.matches('.burger__button') && !e.target.matches('.burger__menu')) {
-        burgerMenu.classList.remove('burger_open');
-    }
-})
+document.querySelectorAll(".places__link").forEach((link) => {
+  if (link.href === window.location.href) {
+    link.classList.add("places__link_active");
+  }
+});
